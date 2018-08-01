c91e43748760feb41ad509f32c432ccd2dff9fa0
alison de amorim <aamorim@acessocard.com.br>
2018-08-01 18:23:58 -0300

    Gateway_Response_Fields

--- Vindi.NET/Model/Gateway_Response_Fields.cs
+++ Vindi.NET/Model/Gateway_Response_Fields.cs
@@ -2,6 +2,13 @@
 {
     public class Gateway_Response_Fields
     {
+        public string id { get; set; }
+        public State state { get; set; }
+        public string print_url { get; set; }
+        public string charge_id { get; set; }
+        public string barcode { get; set; }
+        public string typeable_barcode { get; set; }
         public string nsu { get; set; }
     }
+
 }
---
+++ Vindi.NET/Model/State.cs
@@ -0,0 +1,8 @@
+﻿namespace Vindi.NET.Model
+{
+    public class State
+    {
+        public string name { get; set; }
+        public string code { get; set; }
+    }
+}
