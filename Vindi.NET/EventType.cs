64898d69ac5127a641d1300a308e9b7b8e29d5d7
alison de amorim <aamorim@acessocard.com.br>
2018-08-01 11:33:59 -0300

    Evens Web Hook

---
+++ Vindi.NET/EventType.cs
@@ -0,0 +1,21 @@
+﻿namespace Vindi.NET
+{
+    public enum EventTypes
+    {
+        subscription_canceled,
+        subscription_created,
+        subscription_reactivated,
+        charge_created,
+        charge_refunded,
+        charge_canceled,
+        charge_rejected,
+        bill_created,
+        bill_paid,
+        bill_canceled,
+        bill_seen,
+        period_created,
+        issue_created,
+        payment_profile_created,
+        message_seen
+    }
+}
